4932306dc1b902c39f5d9de6000795c8773e40a8
Joe Watson <joe_watson@mail.com>
2016-07-02 08:30:08 +0100

    Added css etc

--- src/bar-chart.js
+++ src/bar-chart.js
@@ -1,60 +1,93 @@
+/*global d3 rawData */
+
 /*
 
 TODO
-* Scaling
 * Add date to data
-* Axes/labels
 * Mouseover
+* Download data in code
 * Compile to ES5
+* Linting in VS Code?
 
 */
 
-// The data to be visualized
-//var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-//    						11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
-
 let dataset = rawData.data.map((xs) => xs[1]);
 
-console.log(dataset[0], dataset[1]);
-
 // Constants defining how the data is visualized
-const w = 1000;
-const h = 500;
-const barPadding = 0.1;
+const margin = { top: 20, right: 20, bottom: 20, left: 50};
+const svgWidth = 1000;
+const svgHeight = 500;
+const chartWidth = svgWidth - margin.left - margin.right;
+const chartHeight = svgHeight - margin.top - margin.bottom;
+
+const xScale= d3.scale.linear()
+	.domain([0, dataset.length - 1])
+	.range([0, chartWidth]);
+
+const yScale = d3.scale.linear()
+  .domain([d3.min(dataset), d3.max(dataset)])
+  .range([chartHeight, 0]);
+
+const barWidth = chartWidth / dataset.length;
 
 // Create SVG element
-let svg = d3
+const svg =
+	d3
   .select("body")
   .append("svg")
-  .attr("width", w)
-  .attr("height", h);
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
 
-svg
-  .selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("x", (d, i) => i * (w / dataset.length))
-  .attr("y", (d) => h - (d * 0.025))
-  .attr("width", w / dataset.length - barPadding)
-  .attr("height", (d) => d * 0.025)
-  .attr("fill", (d) => "rgb(0, 0, " + (d * 10) + ")");
+// Create a group to contain the chart, and apply the margins
+let chartGroup =
+	svg
+	.append("g")
+	.attr("class", "chart")
+	.attr("transform", translate(margin.left, margin.top));
 
-			// svg.selectAll("text")
-			//    .data(dataset)
-			//    .enter()
-			//    .append("text")
-			//    .text(function(d) {
-			//    		return d;
-			//    })
-			//    .attr("text-anchor", "middle")
-			//    .attr("x", function(d, i) {
-			//    		return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
-			//    })
-			//    .attr("y", function(d) {
-			//    		return h - (d * 4) + 14;
-			//    })
-			//    .attr("font-family", "sans-serif")
-			//    .attr("font-size", "11px")
-			//    .attr("fill", "white");
+// Add x-axis
+const xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient("bottom");
+chartGroup
+	.append("g")
+	.attr("class", "axis x-axis")
+	.attr("transform", translate(0, chartHeight))
+	.call(xAxis);
 
+// Add y-axis
+const yAxis =
+	d3
+	.svg
+	.axis()
+  .scale(yScale)
+  .orient("left");
+chartGroup
+	.append("g")
+  	.attr("class", "axis y-axis")
+    .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6) // Text anchor is about 6 units down from the top of the chart
+      .attr("dy", ".71em") // Makes text anchor point about the cap height (instead of the bottom)
+      .style("text-anchor", "end")
+      .text("USA GDP");
+
+// Add bars
+chartGroup
+	.append("g")
+		.attr("class", "bars")
+  	.selectAll("rect")
+  	.data(dataset)
+		.enter()
+		.append("rect")
+			.attr("x", (d, i) => xScale(i))
+			.attr("y", (d) => yScale(d))
+			.attr("width", barWidth)
+			.attr("height", (d) => chartHeight - yScale(d));
+
+// Helper function
+
+function translate(x, y) {
+	return "translate(" + x + ", " + y + ")";
+}
